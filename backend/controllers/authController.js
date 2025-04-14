85ec6cb46ac7b9217c76a61af2a683e9bf1f909b
singhpraveen77 <singhpraveen8893@gmail.com>
2025-04-14 23:36:44 +0530

    forgetpassword add

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -15,24 +15,24 @@ const registerUser = async (req, res) => {
 
     const avatarLocalPath = req.file?.path;
 
-    if (!avatarLocalPath) {
-        return res.status(400).send("Please upload an avatar image");
-    }
+    // if (!avatarLocalPath) {
+    //     return res.status(400).send("Please upload an avatar image");
+    // }
 
-    const avatar = await uploadOnCloudinary(avatarLocalPath, "avatars")
+    // const avatar = await uploadOnCloudinary(avatarLocalPath, "avatars")
 
-    if (!avatar) {
-        return res.status(500).send("Failed to upload avatar image");
-    }
+    // if (!avatar) {
+    //     return res.status(500).send("Failed to upload avatar image");
+    // }
     
     const user = await User.create({
         name,
         email,
         password,
-        avatar: {
-            public_id: avatar.public_id,
-            url: avatar.url
-        },
+        // avatar: {
+        //     public_id: avatar.public_id,
+        //     url: avatar.url
+        // },
     })
 
     const token = user.getJWTtoken();
@@ -74,4 +74,51 @@ const logOutUser = async (req, res) => {
     res.status(200).json({ message: "Logged out successfully" });
 };
 
-export { registerUser, loginUser ,logOutUser};
+const forgotpassword = async (req, res) => {
+    let  { email } = req.body;
+
+    let user= await User.findOne({ email })
+
+    if(!user){
+        return res.status(404).send("User not found");
+
+
+    }
+
+    const resetToken = user.getResetPasswordToken();
+
+    await user.save({ validateBeforeSave: false });
+
+    const resetPasswordUrl = `${req.protocol}://${req.get("host")}/api/v1/password/reset/${resetToken}`;
+
+    const message = `Your password reset token is as follows:\n\n ${resetPasswordUrl} \n\nIf you have not requested this email, please ignore it.`;
+
+    try{
+        await sendEmail({
+            email: user.email,
+            subject: `Ecommerce password recovery`,
+            message,
+        })
+        
+
+        res.status(200).json({ success: true, message: `Email sent to ${user.email} successfully` });
+    }
+    catch(error){
+        await User.updateOne({
+            _id:user._id
+
+        },
+        {
+            $unset:{
+                resetPasswordToken: "",
+                resetPasswordExpire: ""
+            }
+        })
+        res.status(500).json({ success: false, message:"Error in sending the Email .... please try again later !!" });
+    }
+    
+}
+
+
+export { registerUser, loginUser ,logOutUser,forgotpassword};
+
