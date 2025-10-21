545b05113e8f1ea1d784532257a8f70213618034
singhpraveen77 <singhpraveen8893@gmail.com>
2025-10-21 11:42:07 +0530

    fix: Correct comment formatting for BASE_URL definition in axiosInstance.js

--- frontend/src/axios/axiosInstance.js
+++ frontend/src/axios/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-const BASE_URL = import.meta.env.VITE_BASE_URL ;
+// const BASE_URL = import.meta.env.VITE_BASE_URL ;
 
-// const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:5000/app/v1";
+const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:5000/app/v1";
 
 export const axiosInstance = axios.create({
   baseURL:BASE_URL, // replace with your backend API UR
