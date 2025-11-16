335de84cda346d65eace399f3be93f9f54bbe178
singhpraveen77 <singhpraveen8893@gmail.com>
2025-11-16 11:40:21 +0530

    feat: update BASE_URL configuration to prioritize environment variable over localhost

--- frontend/src/axios/axiosInstance.js
+++ frontend/src/axios/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 // const BASE_URL = import.meta.env.VITE_BASE_URL ;
 
-const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:5000/app/v1";
+const BASE_URL = import.meta.env.VITE_BASE_URL 
 // const BASE_URL ="http://localhost:5000/app/v1";
 
 export const axiosInstance = axios.create({
